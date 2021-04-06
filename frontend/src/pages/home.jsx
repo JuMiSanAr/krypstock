4997a876a05b0968cae3aecf114a853144138088
Cindy Reber <cindy.reber@hispeed.ch>
2021-04-06 21:36:43 +0200

    added button

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,9 +1,11 @@
 import FooterNav from '../components/FooterNav'
+import Button from '../styles/components/buttonStyles'
 
 const Home = () => {
 
     return (
         <>
+            <Button/>
             <FooterNav/>
         </>
     )
