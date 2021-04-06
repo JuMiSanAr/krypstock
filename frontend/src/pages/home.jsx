6b53c7243258a749bee17490834765986beef7f6
Cindy Reber <cindy.reber@hispeed.ch>
2021-04-06 21:49:39 +0200

    button

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,21 +1,14 @@
-<<<<<<< HEAD
-import FooterNav from '../components/FooterNav'
-import Button from '../styles/components/buttonStyles'
-=======
-import FooterNav from '../components/footerNav'
+import Button from '../styles/components/buttonStyles';
+import FooterNav from '../components/footerNav';
 import {HumburgerNavi} from "../components/hamburgerNavi";
->>>>>>> dev
 
 const Home = () => {
 
     return (
         <>
-<<<<<<< HEAD
             <Button/>
-=======
             <HumburgerNavi/>
             <h1>Home</h1>
->>>>>>> dev
             <FooterNav/>
         </>
     )
