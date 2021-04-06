21bb9a96e93ac9b1fdba95da642a68f1cd0a2a95
Kanoknuch Klopfenstein <eve.klopfenstein@gmail.com>
2021-04-06 16:46:52 +0200

    add Home

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,18 +1,14 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { Router } from 'react-router'
-import history from '../history'
-
+import { Router } from 'react-router';
+import history from '../history';
+import Home from '../pages/home';
 
 function OurRouter() {
   return (
     <Router history={history}>
       <Switch>
-        {/* 
         <Route path="/" exact component={Home} />
-        <Route path="/restaurant-page/:id/" exact component={RestaurantPage} />
-        <Route path="/sign-in/" exact component={SignIn} />
-        <Route path="/sign-up/" exact component={SignUp} /> */}
         <Route path="*" component={() => "404 NOT FOUND"} />
       </Switch>
     </Router>
