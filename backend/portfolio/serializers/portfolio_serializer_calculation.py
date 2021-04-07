34f1da07ee202c9f4a1b13305f55da183decdd1b
Juan Miguel Sánchez Arce <jumisan134@gmail.com>
2021-04-07 10:21:12 +0200

    Finish updating calculations

--- backend/portfolio/serializers/portfolio_serializer_calculation.py
+++ backend/portfolio/serializers/portfolio_serializer_calculation.py
@@ -23,24 +23,39 @@ class PortfolioSerializerWithCalculation(serializers.ModelSerializer):
         results = []
 
         for symbol in unique_symbols:
-            total_paid = 0
-            number_bought = 0
+            this_symbol_transactions = [transaction for transaction in transactions if transaction.symbol == symbol]
 
-            for transaction in transactions:
-                if transaction.symbol == symbol and transaction.buy_sell == 'B':
-                    total_paid += float(transaction.quantity) * float(transaction.price)
-                    number_bought += float(transaction.quantity)
-                elif transaction.symbol == symbol and transaction.buy_sell == 'S':
-                    total_paid -= float(transaction.quantity) * float(transaction.price)
-                    number_bought -= float(transaction.quantity)
+            currently_invested = 0
+            number_bought = 0
+            balance = 0
+
+            for transaction in this_symbol_transactions:
+                if transaction.active:
+                    if transaction.buy_sell == 'B':
+                        currently_invested += float(transaction.quantity) * float(transaction.price)
+                        number_bought += float(transaction.quantity)
+                    elif transaction.buy_sell == 'S':
+                        currently_invested -= float(transaction.quantity) * float(transaction.price)
+                        number_bought -= float(transaction.quantity)
+                elif not transaction.active:
+                    if transaction.buy_sell == 'B':
+                        balance -= float(transaction.quantity) * float(transaction.price)
+                    elif transaction.buy_sell == 'S':
+                        balance += float(transaction.quantity) * float(transaction.price)
 
             if number_bought > 0:
-                this_average = total_paid / number_bought
+                this_average = currently_invested / number_bought
 
                 results.append({'symbol': symbol,
-                                'total_invested': total_paid,
+                                'currently_invested': currently_invested,
                                 'quantity': number_bought,
-                                'average_price': this_average})
+                                'average_price': this_average,
+                                'overall_balance': balance})
+            else:
+                for active_transaction in this_symbol_transactions:
+                    active_transaction.active = False
+                    active_transaction.save()
+
         return results
 
     class Meta:
