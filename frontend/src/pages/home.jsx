3beba3ee98bee20116bac5440cb6e8e8d5f9727b
Kanoknuch Klopfenstein <eve.klopfenstein@gmail.com>
2021-04-07 14:57:54 +0200

    just commit

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,23 +1,15 @@
 // import Button from '../styles/components/buttonStyles';
 import {Crypto }from '../components/homeComponents/crypto/index'
 import FooterNav from '../components/footerNav';
-<<<<<<< HEAD
 import Stock from '../components/homeComponents/stock/index.jsx';
-=======
-// import MarketOverview from '../components/homeComponents/marketOverview';
->>>>>>> dev
 
 const Home = () => {
 
     return (
         <>
             {/* <Button/> */}
-<<<<<<< HEAD
             <Stock/>
-=======
-            {/* <MarketOverview/> */}
             <Crypto/>
->>>>>>> dev
             <FooterNav/>
 
         </>
