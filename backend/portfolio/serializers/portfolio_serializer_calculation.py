bad2451bbf6fe0cde970f9e9a049d667e7e08726
Juan Miguel Sánchez Arce <jumisan134@gmail.com>
2021-04-07 10:57:53 +0200

    Finish updating calculations with correct balances

--- backend/portfolio/serializers/portfolio_serializer_calculation.py
+++ backend/portfolio/serializers/portfolio_serializer_calculation.py
@@ -28,31 +28,47 @@ class PortfolioSerializerWithCalculation(serializers.ModelSerializer):
             currently_invested = 0
             number_bought = 0
             balance = 0
+            previous_balance = 0
+
+            active_transactions = []
 
             for transaction in this_symbol_transactions:
                 if transaction.active:
                     if transaction.buy_sell == 'B':
                         currently_invested += float(transaction.quantity) * float(transaction.price)
                         number_bought += float(transaction.quantity)
+                        balance -= float(transaction.quantity) * float(transaction.price)
                     elif transaction.buy_sell == 'S':
                         currently_invested -= float(transaction.quantity) * float(transaction.price)
                         number_bought -= float(transaction.quantity)
+                        previous_balance += float(transaction.quantity) * float(transaction.price)
+                        balance += float(transaction.quantity) * float(transaction.price)
+                    active_transactions.append(transaction)
                 elif not transaction.active:
                     if transaction.buy_sell == 'B':
+                        previous_balance -= float(transaction.quantity) * float(transaction.price)
                         balance -= float(transaction.quantity) * float(transaction.price)
                     elif transaction.buy_sell == 'S':
+                        previous_balance += float(transaction.quantity) * float(transaction.price)
                         balance += float(transaction.quantity) * float(transaction.price)
 
             if number_bought > 0:
                 this_average = currently_invested / number_bought
 
                 results.append({'symbol': symbol,
-                                'currently_invested': currently_invested,
+                                'invested': currently_invested,
                                 'quantity': number_bought,
                                 'average_price': this_average,
-                                'overall_balance': balance})
+                                'previous_balance': previous_balance})
             else:
-                for active_transaction in this_symbol_transactions:
+
+                results.append({
+                    'symbol': symbol,
+                    'invested': 0,
+                    'overall_balance': balance
+                })
+
+                for active_transaction in active_transactions:
                     active_transaction.active = False
                     active_transaction.save()
 
