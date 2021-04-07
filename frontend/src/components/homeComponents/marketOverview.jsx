321ee95bad1646e8c18123763f10b37fccaad726
Kanoknuch Klopfenstein <eve.klopfenstein@gmail.com>
2021-04-07 10:10:32 +0200

    just commit

--- frontend/src/components/homeComponents/marketOverview.jsx
+++ frontend/src/components/homeComponents/marketOverview.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AllComponentsWrapper, ShrinkingComponentWrapper } from '../../styles/globalParts/containerStyles';
 import Graph from '../../assets/stock_graph.png'
+
 const MarketOverview = () => {
 
     return (
--- frontend/src/styles/globalParts/containerStyles.js
+++ frontend/src/styles/globalParts/containerStyles.js
@@ -5,20 +5,22 @@ export const AllComponentsWrapper = styled.div`
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-    border: solid 1px purple;
+    /* border: solid 1px purple; */
 `
 
-export const ShrinkingComponentWrapper = styled.div`
+export const FullWidthComponentWrapper = styled.div`
     width: 90%;
     height: 350px;
-    /* dont forget to change the height */
-    border: solid 1px #3B3363;
+/* dont forget to change the height */
+    border: solid 1px #3B3363; 
     margin: 1rem; 
-
     img {
         width: 80%;     
     }
+`
 
+
+export const ShrinkingComponentWrapper = styled(FullWidthComponentWrapper)`
     @media (min-width: 450px) {
         width: 40%;
     }
