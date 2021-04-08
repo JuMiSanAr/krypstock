77e28714689aad2aeb81b401762539fab74d4730
Juan Miguel Sánchez Arce <jumisan134@gmail.com>
2021-04-08 12:27:20 +0200

    With premium membership (woooo)

--- frontend/src/components/newsFeed/newsStock.jsx
+++ frontend/src/components/newsFeed/newsStock.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {ShrinkingComponentWrapper} from "../../styles/globalParts/containerStyles";
 import {NewsWrapper} from "../../styles/components/stockStyles/newsStyles";
+import {iexAPIKey, iexSandboxKey} from "../../store/constants";
 
 const NewsStock = (props) => {
 
@@ -14,8 +15,7 @@ const NewsStock = (props) => {
     }, []);
 
     const fetchNews = () => {
-        const API_KEY = 'Tpk_fec97062db224c2fb7b0b3836ab0e365';
-        const API_Call = `https://sandbox.iexapis.com/stable/stock/${symbol}/news/last/${last}?token=${API_KEY}`;
+        const API_Call = `https://sandbox.iexapis.com/stable/stock/${symbol}/news/last/${last}?token=${iexSandboxKey}`;
 
         fetch(API_Call)
             .then(res => res.json())
@@ -28,10 +28,27 @@ const NewsStock = (props) => {
         <>
             <h3>Latest news about {symbol}</h3>
             {allNews.length > 0 ? allNews.map((news, index) => {
+                console.log(news)
+
+                const date = new Date(news.datetime);
+
+                const dateOptions = {
+                    weekday: 'long',
+                    year: "numeric",
+                    month: "long",
+                    day: "numeric",
+                    hour: "2-digit",
+                    minute: "2-digit"
+                }
+
+                const stringDate = date.toLocaleString('en-US', dateOptions);
+
                 return (
                     <ShrinkingComponentWrapper key={index}>
                         <NewsWrapper>
                             <h3>{news.headline}</h3>
+                            <p className='news_date'>{stringDate} - By {news.source}</p>
+                            <a href={news.url} target='_blank' rel='noreferrer'>Click here to see the complete article</a>
                         </NewsWrapper>
                     </ShrinkingComponentWrapper>
                 )
