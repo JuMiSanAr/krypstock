7c79ddc1c28dc5bed5d392ed52c8a1bc9275e744
Domenico Dongiovanni <domenicodongiovanni@Domenicos-iMac.local>
2021-04-08 08:21:51 +0200

    crypto stream

---
+++ frontend/src/components/crypto/index 2.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import FooterNav from '../footerNav';
+import {AllComponentsWrapper, ShrinkingComponentWrapper} from "../../styles/globalParts/containerStyles";
+import {OverviewContainer, DetailFont} from "../../styles/components/cryptoStyles";
+import {FormSelectWrapper} from "../../styles/components/cryptoStyles/bitCoinStyles";
+
+const Crypto = () => {
+
+    return (
+        <>
+             <AllComponentsWrapper>
+
+                   <ShrinkingComponentWrapper>
+                       <h3>Bitcoin</h3 >
+                    <DetailFont>Coin Detail</DetailFont>
+                        <FormSelectWrapper>
+               <div className="title">
+                </div>
+                <div >
+                    <select className="selector">
+                    <option value="month">Month</option>
+                    <option value="week">Week</option>
+                    <option value="day">Day</option>
+                    </select>
+                </div>
+           </FormSelectWrapper>
+                </ShrinkingComponentWrapper>
+            </AllComponentsWrapper>
+            <FooterNav />
+        </>
+    )
+}
+
+export default Crypto;
---
+++ frontend/src/styles/components/cryptoStyles 2.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const OverviewContainer = styled.div`
+display: flex;
+justify-content: space-around;
+background-color: #05a49e;
+border-radius: 14px;
+margin: 10px 0;
+`;
+
+export const CryptoContainer = styled.div`
+display: flex;
+justify-content: flex-start;
+border: solid 1px red;
+`;
+
+export const HeadlineFont = styled.p`
+font-weight: 1000;
+`;
+
+export const DetailFont = styled.p`
+font-weight: 400;
+font-size: 15px;
+`;
