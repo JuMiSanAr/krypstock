03fbb8746070bbf9ee1d45d700a27212d2304707
Domenico Dongiovanni <domenicodongiovanni@Domenicos-iMac.local>
2021-04-15 18:08:00 +0200

    overview buy/sell button

--- frontend/src/components/portfolioComponents/overview.jsx
+++ frontend/src/components/portfolioComponents/overview.jsx
@@ -1,10 +1,18 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import { ShrinkingComponentWrapper } from '../../styles/globalParts/containerStyles';
 import {HeadlineFont, Headline, OverviewBar, TempDiv, Desc, NetworthContainer, IconConatiner} from '../../styles/components/portfolioStyles';
 import {useSelector} from 'react-redux';
 import {allTheme} from '../../styles/Themes';
+import {StockModal2} from "../quickTradeModal/stockOverview";
+import {CryptoModal2} from "../quickTradeModal/cryptoOverview";
+
 
 const Overview = ({calculations}) => {
+
+    const [showStockModal, setStockShowModal] = useState(false);
+    const [showCryptoModal, setCryptoShowModal] = useState(false);
+    const [symbolCrypto, setSymbolCrypto] = useState();
+    const [stockSymbol, setStockSymbol] = useState();
 
     let colors = [allTheme.vibrantturquoise, allTheme.darkblue, allTheme.yellow, allTheme.vibrantorange];
     let currentColor = -1;
@@ -19,9 +27,10 @@ const Overview = ({calculations}) => {
         }
         return colors[currentColor];
     }
-
-    return (
-        <ShrinkingComponentWrapper>
+    return (<>
+            <CryptoModal2  symbol = {`${symbolCrypto}`} showCryptoModal={showCryptoModal} setCryptoShowModal={setCryptoShowModal}/>
+            <StockModal2 stockSymbol={stockSymbol}  symbol = {stockSymbol} showStockModal={showStockModal} setStockShowModal={setStockShowModal}/>
+            <ShrinkingComponentWrapper>
             <Headline>Overview</Headline>
             {calculations.map((calculation, index) =>
             {if (calculation.invested > 0) {
@@ -33,6 +42,14 @@ const Overview = ({calculations}) => {
                     : <i className="fab fa-btc"></i>
                     }
                     <HeadlineFont>{calculation.symbol}</HeadlineFont>
+                    {calculation.type === "S"
+                    ? <>
+                            <button onClick={()=>{setStockShowModal(true);setStockSymbol(calculation.symbol);}}>BUY/SELL</button>
+                      </>
+                    : <>
+                            <button onClick={()=>{setCryptoShowModal(true);setSymbolCrypto(calculation.symbol);}}>BUY/SELL</button>
+                      </>
+                    }
                 </IconConatiner>
                 <NetworthContainer>
                     <TempDiv>
@@ -54,6 +71,7 @@ const Overview = ({calculations}) => {
             </OverviewBar>
             )}})}
         </ShrinkingComponentWrapper>
+        </>
 )}
 
 export default Overview;
