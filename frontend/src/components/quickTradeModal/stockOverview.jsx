84fb47d4f0472d823aa33addfca6ef1754ff7c2c
Domenico Dongiovanni <domenicodongiovanni@Domenicos-iMac.local>
2021-04-16 17:35:28 +0200

    added market price

--- frontend/src/components/quickTradeModal/stockOverview.jsx
+++ frontend/src/components/quickTradeModal/stockOverview.jsx
@@ -1,10 +1,12 @@
-import React, { useRef, useState } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import { Background, CloseModalButton, ContentWrapper, ModalContent, CryptStockFormSelectWrapper, CrypStockTransacWrapper } from '../../styles/components/modalStyles';
 import {useSelector} from "react-redux";
-import { ButtonWrapper} from '../../styles/components/cryptoStyles/quickTradeStyles'
+import {ButtonWrapper, TransacWrapper} from '../../styles/components/cryptoStyles/quickTradeStyles'
 import { Link } from 'react-router-dom';
 import {ShrinkingComponentWrapper } from '../../styles/globalParts/containerStyles';
 import { postNewTransactionFetch } from '../../store/fetches/transactionFetches';
+import {iexSandboxKey} from "../../store/constants";
+import {ErrorSpan} from "../../styles/globalParts/textStyles";
 
 export const StockModal2 = ({ showStockModal, setStockShowModal, symbol, stockSymbol,portfolioname, portfolioID }) => {
     const allPortfoliosArray = useSelector(state => state.portfoliosReducer.portfolios)
@@ -12,17 +14,26 @@ export const StockModal2 = ({ showStockModal, setStockShowModal, symbol, stockSy
     const [buySell, setBuySell] = useState();
     const [volume, setVolume] = useState();
     const [pricePerShare, setPricePerShare] = useState();
+    const [marketPrice, setMarketPrice] = useState();
+     const [notEnoughStocks, setNotEnoughStocks] = useState(false);
     const type = "S";
 
-    // console.log("symbol", stockSymbol)
     const submitHandler = (e) => {
         e.preventDefault();
         // console.log(buySell, portfolioID, symbol, volume, pricePerShare,type)
         postNewTransactionFetch(buySell, portfolioID, stockSymbol, volume, pricePerShare, type)
         .then(data => {
+            setStockShowModal(false);
             console.log('in stock quicktrade submitHandler', data)
         })
-        setStockShowModal(false);
+        .catch(error => {
+                // console.log(error.split('')[error.length-1])
+                if (error.toString().slice(-1) === '3') {
+                    console.log('error', error)
+                    console.log("You don't have enough coins to sell")
+                    setNotEnoughStocks(true);
+                }
+            })
     }
 
   const modalRef = useRef();
@@ -34,6 +45,21 @@ export const StockModal2 = ({ showStockModal, setStockShowModal, symbol, stockSy
     }
   };
 
+  useEffect( () => {
+            fetch(`https://sandbox.iexapis.com/stable/stock/${symbol}/price?token=${iexSandboxKey}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log("useState ~ data", data)
+                setMarketPrice(data)
+            })
+            .catch( error => {console.log('error', error)})
+
+    }, [symbol, buySell])
+
+    useEffect( () => {
+        setNotEnoughStocks(false)
+    }, [symbol, buySell, portfolioID, volume])
+
   return (
     <>
       {showStockModal ? (
@@ -41,25 +67,14 @@ export const StockModal2 = ({ showStockModal, setStockShowModal, symbol, stockSy
             <Background onClick={closeModal} ref={modalRef}>
             <ContentWrapper>
             <ShrinkingComponentWrapper showStockModal={showStockModal}>
-                <ModalContent> 
-                {/* <form onSubmit={submitHandler}> */}
-                <form>
+                <ModalContent>
                 <CryptStockFormSelectWrapper>
-                    <div className="title">
-                       <h4>Stock Quick Trade</h4> 
-                    </div>
-                    {
-                    !allPortfoliosArray || allPortfoliosArray.length === 0 ?
-                    null
-                    :
-                        <div className="buySell">
-                            <select className="selector" defaultValue={'DEFAULT'} onChange={e => setBuySell(e.target.value)} required>
-                                <option value="DEFAULT" disabled>Select</option>
-                                <option value="B">Buy</option>
-                                <option value="S">Sell</option>
-                            </select>
+                        <div>
+                            <button value="B" onClick={e => setBuySell(e.target.value)}>BUY</button>
                         </div>
-                    }
+                        <div>
+                            <button value="S" onClick={e => setBuySell(e.target.value)}>SELL</button>
+                        </div>
                 </CryptStockFormSelectWrapper>  
                 {
                     !allPortfoliosArray || allPortfoliosArray.length === 0 ?
@@ -105,6 +120,10 @@ export const StockModal2 = ({ showStockModal, setStockShowModal, symbol, stockSy
                                <input className="input" type="number" placeholder="0" value={pricePerShare} onChange={e => setPricePerShare(e.target.value)} required />
                                </div>  
                             </div>
+                             <div className="amountInput">
+                                <p>Market Price</p>
+                                <span>{`${marketPrice}  USD`}</span>
+                            </div>
                             <div className="amountInput">
                                 <div>
                                 <p>Total Price</p>
@@ -113,14 +132,15 @@ export const StockModal2 = ({ showStockModal, setStockShowModal, symbol, stockSy
                                 <span>{`${volume*pricePerShare ? parseFloat(volume*pricePerShare).toFixed(2) : '0.00' }  USD`}</span>
                                 </div>   
                             </div>
-                        </CrypStockTransacWrapper> 
+                        </CrypStockTransacWrapper>
+                        {
+                            notEnoughStocks ? <ErrorSpan><em>Not enough stocks to sell at this amount</em></ErrorSpan> : ''
+                        }
                         <ButtonWrapper>
-                            {/* <button type="submit" value="Submit">Submit</button> */}
                             <button type="submit" value="Submit" onClick={submitHandler}>Submit</button>
                         </ButtonWrapper>
                     </>
                 }
-            </form>
                 </ModalContent>
                 <CloseModalButton
                 aria-label='Close modal'
