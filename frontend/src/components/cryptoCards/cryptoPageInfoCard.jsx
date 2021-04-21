5af627c2c80eeaba83741909d093d1de2f1d4782
Domenico Dongiovanni <domenicodongiovanni@Domenicos-iMac.local>
2021-04-21 15:42:45 +0200

    test

--- frontend/src/components/cryptoCards/cryptoPageInfoCard.jsx
+++ frontend/src/components/cryptoCards/cryptoPageInfoCard.jsx
@@ -11,13 +11,16 @@ const CryptoPageInfoCard = (props) => {
     // let newString = 'has a current supply of 1,028,883,390.2296 '.replace(/([^\d]*)(\d*)([^\w]*)/, replacer);
     //  console.log(newString);
 
-     const St = "EOS has a current supply of 1,028,883,390.2296 with ";
+     const St = "EOS (EOS) is a cryptocurrency . EOS has a current supply of 1,028,883,390.2296 with 952,716,312.0057 in circulation. The last known price of EOS is 6.59516367 USD and is up 8.27 over the last 24 hours. It is currently trading on 477 active market(s) with $3,627,802,283.44 traded over the last 24 hours. More information can be found at https://eos.io/.";
 
-const pFrom = St.indexOf("has a current supply of") + "supply of".length;
-const  pTo = St.lastIndexOf(" - ");
+const supplyFrom = St.indexOf("supply of", 0);
+const withFrom = St.indexOf("with", 0);
+const circulationFrom = St.indexOf("in circulation", 0);
+const isFrom = St.indexOf("is", circulationFrom);
+// const  pTo = St.lastIndexOf("EOS has a current supply ");
 
-const result = St.substring(pFrom, pTo - pFrom);
-console.log(pFrom,pTo)
+// const result = St.substring(pFrom, pTo - pFrom);
+console.log(supplyFrom,withFrom,circulationFrom,isFrom)
 
 //     function replacer(match, p1, p2, p3, offset, string) {
 //   // p1 is nondigits, p2 digits, and p3 non-alphanumerics
