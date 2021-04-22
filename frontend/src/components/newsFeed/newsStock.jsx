1f566c109bf5279410949f93675a86aa83c96cf9
Juan Miguel Sánchez Arce <jumisan134@gmail.com>
2021-04-22 21:23:38 +0200

    Preparing for the big day

--- frontend/src/components/newsFeed/newsStock.jsx
+++ frontend/src/components/newsFeed/newsStock.jsx
@@ -19,7 +19,7 @@ const NewsStock = (props) => {
     };
 
     useEffect(() => {
-        fetchNews();
+        // fetchNews();
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
